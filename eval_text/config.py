f36087211c42b71b005dc782a0fe995ef68f2a5b
pengshuai.rin2 <pengshuai.rin2@bytedance.com>
2024-09-02 19:27:42 +0800

    update

--- eval_text/config.py
+++ eval_text/config.py
@@ -1,23 +1,17 @@
 dataset_config = {
     "gsm8k": {
-        "train_path": "/mnt/bn/pengshuai-nas/data/MathDataset/gsm8k/train.jsonl",
-        "test_path": "/mnt/bn/pengshuai-nas/data/MathDataset/gsm8k/test.jsonl",
+        "train_path": "./playground/gsm8k/train.jsonl",
+        "test_path": "./playground/gsm8k/test.jsonl",
     },
     "math": {
-        "train_path": "/mnt/bn/pengshuai-nas/data/MathDataset/math/train.jsonl",
-        "test_path": "/mnt/bn/pengshuai-nas/data/MathDataset/math/test.jsonl",
+        "train_path": "./playground/math/train.jsonl",
+        "test_path": "./playground/math/test.jsonl",
     },
     "cmath": {
-        "dev_path": "/mnt/bn/pengshuai-nas/data/MathDataset/cmath/dev.jsonl",
-        "test_path": "/mnt/bn/pengshuai-nas/data/MathDataset/cmath/test.jsonl",
-    },
-    "mgsm_zh": {
-        "test_path": "/mnt/bn/pengshuai-nas/data/MathDataset/mgsm_zh/mgsm_zh.jsonl"
+        "dev_path": "./playground/cmath/dev.jsonl",
+        "test_path": "./playground/cmath/test.jsonl",
     },
     "gaokao-mathcloze": {
-        "test_path": "/mnt/bn/pengshuai-nas/data/MathDataset/gaokao-mathcloze/gaokao-mathcloze.jsonl"
+        "test_path": "./playground/gaokao-mathcloze/gaokao-mathcloze.jsonl"
     },
-    "gaokao-mathqa": {
-        "test_path": "/mnt/bn/pengshuai-nas/data/MathDataset/gaokao-mathqa/gaokao-mathqa.jsonl"
-    }
 }
